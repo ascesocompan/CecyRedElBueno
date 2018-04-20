c39154adb4d88e24bdefe0859298bd3be22d4e26
ascesocompan <antonydotcom@hotmail.com>
2018-04-19 20:00:49 -0500

    Solo agregue un boton de cerrar sesion

---
+++ app/src/main/java/com/cecyred/ww/cecyred/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.cecyred.ww.cecyred;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cecyred.ww.cecyred.R;
+
+/**
+ * Created by User on 18/04/2018.
+ */
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+}
