86d4b9ddc4e310b80f2bbb2f33c5527855ba360f
Carlos <carloslego.br@gmail.com>
2018-04-20 22:28:31 -0500

    se añade registro(version de prueba)

--- app/src/main/java/com/cecyred/ww/cecyred/REGISTROUSUARIO.java
+++ app/src/main/java/com/cecyred/ww/cecyred/REGISTROUSUARIO.java
@@ -1,5 +1,6 @@
 package com.cecyred.ww.cecyred;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,26 +8,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class REGISTROUSUARIO extends AppCompatActivity {
+EditText Et_nom,Et_pass,Et_bol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrousuario);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Cecyred");*/
-
-
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       Et_nom=(EditText)findViewById(R.id.TXT_NOMBRE);
+       Et_pass=(EditText)findViewById(R.id.TXT_CONTRA);
+       Et_bol=(EditText)findViewById(R.id.TXT_BOLETA);
+    }
+    public void registro(View v){
+        String usuario=Et_nom.getText().toString();
+        String contrasena=Et_pass.getText().toString();
+        String boleta=Et_bol.getText().toString();
+        Response.Listener<String> respolistener= new Response.Listener<String>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onResponse(String response) {
+                JSONObject jsonResponse= null;
+                try {
+                    jsonResponse = new JSONObject(response);
+                    boolean succes=jsonResponse.getBoolean("success");
+                    if(succes){
+                        Intent intent=new Intent(REGISTROUSUARIO.this,LogginActivity.class);
+                        REGISTROUSUARIO.this.startActivity(intent);
+                    }else{
+
+                        Toast.makeText(getApplicationContext(), "Ocurrio un error en el registro", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                   //e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), ""+e.toString(), Toast.LENGTH_LONG).show();
+                }
+
             }
-        });*/
+        };
+        requestregistro requestreg = new requestregistro(usuario,contrasena,boleta,respolistener);
+        RequestQueue queue= Volley.newRequestQueue(REGISTROUSUARIO.this);
+        queue.add(requestreg);
+
     }
 
 }
---
+++ app/src/main/java/com/cecyred/ww/cecyred/requestregistro.java
@@ -0,0 +1,36 @@
+package com.cecyred.ww.cecyred;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Carlos on 20/04/2018.
+ */
+
+public class requestregistro extends StringRequest {
+    private static final String REGISTER_REQUEST_URL="http://cecyred.com.mx/registro.php";
+    private Map<String,String> params;
+    public requestregistro(String usuario, String contrasena, String boleta, Response.Listener<String> listener) {
+        super(Method.POST,REGISTER_REQUEST_URL, listener, null);
+        params=new HashMap<>();
+        params.put("usuario",usuario);
+        params.put("contrasena", contrasena);
+        params.put("boleta", boleta);
+
+    }
+
+    /*public requestregistro(String usuario, String contrasena, String boleta, Listener<String> respolistener) {
+        super();
+    }*/
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+
+
+}
