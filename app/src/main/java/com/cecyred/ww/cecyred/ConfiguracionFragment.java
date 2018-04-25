d66c523a89eac315adf380bb29b374120845c4fa
Carlos <carloslego.br@gmail.com>
2018-04-24 21:43:04 -0500

    se agregan iconos d etodas dimensiones, faltan radiales, y se agrega textinput-password en registro

--- app/src/main/java/com/cecyred/ww/cecyred/ConfiguracionFragment.java
+++ app/src/main/java/com/cecyred/ww/cecyred/ConfiguracionFragment.java
@@ -1,5 +1,6 @@
 package com.cecyred.ww.cecyred;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -17,4 +18,5 @@ public class ConfiguracionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_configuracion,container,false);
     }
+
 }
