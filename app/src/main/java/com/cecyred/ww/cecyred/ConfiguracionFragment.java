345295ce7b3379c5dc31b850ade4841b2eadef4e
Carlos <carloslego.br@gmail.com>
2018-04-28 22:46:58 -0500

    se cambia el icono en todas sus dimensiones

--- app/src/main/java/com/cecyred/ww/cecyred/ConfiguracionFragment.java
+++ app/src/main/java/com/cecyred/ww/cecyred/ConfiguracionFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 public class ConfiguracionFragment extends Fragment {
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_configuracion,container,false);
     }
